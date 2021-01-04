418b450994e09f4c156b26e202497b2d472521b3
Joel Ek <contact@joelek.se>
2021-01-04 14:21:48 +0100

    xmi parser

--- source/server/index.ts
+++ source/server/index.ts
@@ -137,14 +137,105 @@ function xmi2mid(source: string, target: string): void {
 			{
 				options.cursor = 0
 				let buffer = Buffer.from(evnt.data);
+				let g_channel = 0;
+				let g_tempo = 500000;
 				while (options.cursor < buffer.length) {
 					let byte = buffer.readUInt8(options.cursor); options.cursor += 1;
-					if (byte === 0xFF) {
-						let type = buffer.readUInt8(options.cursor); options.cursor += 1;
-						let size = readVarlen(buffer, options);
-						let data = buffer.slice(options.cursor, options.cursor + size); options.cursor += size;
-						if (type === 0x58) {
-							console.log(`Time signature ${data[0]}/${1 << data[1]}ths, ${data[2]} clocks/4th, ${data[3]} 32nd/4ths`);
+					let delay = 0;
+					if (byte < 0x80) {
+						options.cursor -= 1;
+						while (true) {
+							byte = buffer.readUInt8(options.cursor); options.cursor += 1;
+							delay += byte;
+							if (byte < 0x7F) {
+								break;
+							}
+						}
+						byte = buffer.readUInt8(options.cursor); options.cursor += 1;
+					}
+					if (delay > 0) {
+						console.log("Delay", delay);
+					}
+					let event = (byte >> 4) & 0x0F;
+					let channel = (byte >> 0) & 0x0F;
+					if (event < 0x08) {
+						throw `Invalid event ${event} @ ${options.cursor}`;
+					} else if (event === 0x8) {
+						console.log("Note off");
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+						let b = buffer.readUInt8(options.cursor); options.cursor += 1;
+					} else if (event === 0x9) {
+						console.log("Note on", options.cursor);
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+						let b = buffer.readUInt8(options.cursor); options.cursor += 1;
+						let ticks = readVarlen(buffer, options);
+					} else if (event === 0xA) {
+						console.log("Polyphonic key pressure");
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+						let b = buffer.readUInt8(options.cursor); options.cursor += 1;
+					} else if (event === 0xB) {
+						console.log("Controller");
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+						let b = buffer.readUInt8(options.cursor); options.cursor += 1;
+					} else if (event === 0xC) {
+						console.log("Instrument change");
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+					} else if (event === 0xD) {
+						console.log("Channel pressure");
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+					} else if (event === 0xE) {
+						console.log("Pitch bend");
+						let a = buffer.readUInt8(options.cursor); options.cursor += 1;
+						let b = buffer.readUInt8(options.cursor); options.cursor += 1;
+					} else if (event === 0xF) {
+						if (channel < 0xF) {
+							console.log("Sysex");
+							let size = readVarlen(buffer, options);
+							let data = buffer.slice(options.cursor, options.cursor + size); options.cursor += size;
+						} else {
+							console.log("Meta");
+							let type = buffer.readUInt8(options.cursor); options.cursor += 1;
+							let size = readVarlen(buffer, options);
+							let data = buffer.slice(options.cursor, options.cursor + size); options.cursor += size;
+							if (false) {
+							} else if (type === 0x00) {
+								console.log("\tSequence number", data);
+							} else if (type >= 0x01 && type <= 0x0F) {
+								console.log("\tText", data.toString("binary"));
+							} else if (type === 0x20) {
+								console.log("\tSet active channel", data);
+								let channel = data.readUInt8(0);
+								if (!(channel >= 0 && channel <= 15)) {
+									throw `Invalid channel!`;
+								}
+								g_channel = channel;
+							} else if (type === 0x2F) {
+								console.log("\tEnd of track", data);
+							} else if (type === 0x51) {
+								console.log("\tSet tempo", data);
+								let a = data.readUInt8(0);
+								let b = data.readUInt8(1);
+								let c = data.readUInt8(2);
+								let tempo = (a << 16) | (b << 8) | (c << 0);
+								g_tempo = tempo;
+							} else if (type === 0x54) {
+								console.log("\tSet SMPTE offset", data);
+								let hours = data.readUInt8(0);
+								let minutes = data.readUInt8(1);
+								let seconds = data.readUInt8(2);
+								let frames = data.readUInt8(3);
+								let fractional_frames = data.readUInt8(4) / 100;
+							} else if (type === 0x58) {
+								console.log("\tTime signature", data);
+								let numerator = data.readUInt8(0);
+								let denominator = (1 << data.readUInt8(1));
+								let clocks_per_metronome_click = data.readUInt8(2);
+								let quarter_32nd_notes = data.readUInt8(3); // quarter is 24 clocks
+							} else if (type === 0x7F) {
+								console.log("\tSequencer specific", data);
+							} else {
+								console.log("\tUnrecognized", data);
+							}
 						}
 					}
 				}

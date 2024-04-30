c44f995cea3595a5766c413dd00e6af01062bc9b
Joel Ek <contact@joelek.se>
2024-04-30 07:21:50 +0200

    Write h4 records to disk.

--- source/shared/formats/h4/index.test.ts
+++ source/shared/formats/h4/index.test.ts
@@ -1,6 +1,13 @@
 import * as fs from "fs";
+import * as zlib from "zlib";
 import { Archive } from "./";
 
-let array = Uint8Array.from(fs.readFileSync("./private/h4/text.h4r"));
+let array = Uint8Array.from(fs.readFileSync("./private/h4/music.h4r"));
 let archive = Archive.load(array);
 console.log(archive);
+for (let record_header of archive.record_headers) {
+	let compressed = array.subarray(record_header.offset, record_header.offset + record_header.compressed_size);
+	let uncompressed = zlib.gunzipSync(compressed);
+	fs.mkdirSync(`./private/h4/music/`, { recursive: true });
+	fs.writeFileSync(`./private/h4/music/${record_header.file_name}`, uncompressed);
+}

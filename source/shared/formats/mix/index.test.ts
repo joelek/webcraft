599ad6df7e55d36f44e5fcb8c266f15c4a5bceda
Joel Ek <contact@joelek.se>
2024-05-13 20:15:31 +0200

    Remove aud to wav conversion.

--- source/shared/formats/mix/index.test.ts
+++ source/shared/formats/mix/index.test.ts
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-import * as cp from "child_process";
 import { Archive } from "./";
 
 let array = Uint8Array.from(fs.readFileSync("./private/cnc/SCORES.MIX"));
@@ -8,10 +7,5 @@ console.log(archive);
 for (let [index, record_header] of archive.record_headers.entries()) {
 	let uncompressed = array.subarray(archive.header_size + record_header.relative_offset, archive.header_size + record_header.relative_offset + record_header.length);
 	fs.mkdirSync(`./private/cnc/scores/`, { recursive: true });
-	fs.writeFileSync(`./private/cnc/scores/${index}`, uncompressed);
-	cp.execFileSync("ffmpeg", [
-		"-i", `./private/cnc/scores/${index}`,
-		`./private/cnc/scores/${index}.wav`,
-		"-y"
-	]);
+	fs.writeFileSync(`./private/cnc/scores/${index.toString().padStart(2, "0")}.aud`, uncompressed);
 }
